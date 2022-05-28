8871b0228d7fbd870ac77dceb6259471e56b4e79
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-05-28 22:25:02 +0530

    🧩 feat: add nav

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import styles from './index.module.scss'
+import Logo from '../../public/logo.svg'
+import Call from '../../public/call.svg'
+import Email from '../../public/email.svg'
 
 const Navbar = () => {
-  return <div>NAV</div>
+  return (
+    <nav className={styles.wrapper}>
+      <ul>
+        <li className={styles.logo}>
+          <Logo />
+        </li>
+        <li>
+          <ul>
+            <li datatype='text'>
+              <Call />
+              +769 586 4558
+            </li>
+            <li datatype='text'>
+              <Email />
+              service@openauto.ca
+            </li>
+            <li>
+              <button>Download the mobile app</button>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
